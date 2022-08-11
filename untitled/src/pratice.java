63d24d7858f61eca14b12c70b32dd3cbe83dcd70
Sachin-4619 <sachin.soni@infostride.com>
2022-08-11 13:36:08 +0530

    first commit

---
+++ untitled/src/pratice.java
@@ -0,0 +1,11 @@
+public class pratice {
+    public static void main(String[] args){
+        int i,j,k,l,m,n;
+        for (i=0; i<5; i++)
+        {
+            for(j=0;j<5;j++)
+
+        }
+
+    }
+}
